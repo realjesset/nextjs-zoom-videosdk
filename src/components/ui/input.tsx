507af0cd314e214d08eaca81a31c0aedb9f7f619
Jesse Thomas <realjesset@gmail.com>
2023-06-10 17:29:56 +0400

    chore(pnpm): add shadcn

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "@/utils/cn";
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "input-bordered input h-10 w-full text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed",
+          props.disabled && "input-disabled",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
