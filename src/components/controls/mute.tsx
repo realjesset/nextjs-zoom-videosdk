2bea117ff6bb0600a79d73df0126cccb8c80d282
Jesse Thomas <realjesset@gmail.com>
2023-06-14 18:06:03 +0400

    feat: add mute btn

---
+++ src/components/controls/mute.tsx
@@ -0,0 +1,47 @@
+import useControlStore, { AudioErrorState } from "@/stores/control";
+import cn from "@/utils/cn";
+import React from "react";
+import { BiMicrophone, BiMicrophoneOff } from "react-icons/bi";
+
+const MuteControl = () => {
+  const control = useControlStore(
+    ({ isAudioMuted, toggleAudio, audioError }) => ({
+      isAudioMuted,
+      toggleAudio,
+      audioError,
+    })
+  );
+
+  const onClick = async () => {
+    await control.toggleAudio();
+  };
+
+  const permissionError =
+    control.audioError === AudioErrorState.PERMISSION_DENIED;
+
+  return (
+    <div
+      className="tooltip tooltip-top"
+      data-tip={
+        permissionError ? "Please allow microphone permission" : undefined
+      }
+    >
+      <button
+        className={cn("btn-circle btn", {
+          "btn-neutral": !permissionError && !control?.isAudioMuted,
+          "btn-primary": !permissionError && control?.isAudioMuted,
+          "btn-disabled": permissionError,
+        })}
+        onClick={() => void onClick()}
+      >
+        {control?.isAudioMuted ? (
+          <BiMicrophoneOff className="text-2xl" />
+        ) : (
+          <BiMicrophone className="text-2xl" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default MuteControl;
